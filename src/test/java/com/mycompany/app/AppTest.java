010d2495ef823d121da8a8bb0d5632f9a9ac50df
CameliaZalum <61459440+CameliaZalum@users.noreply.github.com>
2021-03-24 12:13:53 +0200

    first commit

---
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,74 @@
+package com.mycompany.app;
+
+import static org.junit.Assert.assertTrue;
+
+import javax.sound.sampled.SourceDataLine;
+
+import Exceptions.ValidatorException;
+import Repository.XMLFileRepository.NotaXMLRepo;
+import Repository.XMLFileRepository.StudentXMLRepo;
+import Repository.XMLFileRepository.TemaLabXMLRepo;
+import Service.XMLFileService.NotaXMLService;
+import Service.XMLFileService.StudentXMLService;
+import Service.XMLFileService.TemaLabXMLService;
+import UI.ui;
+import Validator.NotaValidator;
+import Validator.StudentValidator;
+import Validator.TemaLabValidator;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void addStudentTestCase1()
+    {
+        StudentValidator vs=new StudentValidator();
+        TemaLabValidator vt=new TemaLabValidator();
+        NotaValidator vn=new NotaValidator();
+        ui.vs = vs;
+        ui.vt = vt;
+        ui.vn = vn;
+        StudentXMLRepo strepo=new StudentXMLRepo(vs,"StudentiXML.xml");
+        TemaLabXMLRepo tmrepo=new TemaLabXMLRepo(vt,"TemaLaboratorXML.xml");
+        NotaXMLRepo ntrepo=new NotaXMLRepo(vn,"NotaXML.xml");
+        StudentXMLService stsrv=new StudentXMLService(strepo);
+        TemaLabXMLService tmsrv=new TemaLabXMLService(tmrepo);
+        NotaXMLService ntsrv=new NotaXMLService(ntrepo);
+        String[] params={"22","nume1","1","email@mail.com","prof"};
+        try {
+            stsrv.add(params);
+            assertTrue(true);
+        } catch (ValidatorException e){
+            assertTrue(false);
+        }
+    }
+    @Test
+    public void addStudentTestCase2()
+    {
+        StudentValidator vs=new StudentValidator();
+        TemaLabValidator vt=new TemaLabValidator();
+        NotaValidator vn=new NotaValidator();
+        ui.vs = vs;
+        ui.vt = vt;
+        ui.vn = vn;
+        StudentXMLRepo strepo=new StudentXMLRepo(vs,"StudentiXML.xml");
+        TemaLabXMLRepo tmrepo=new TemaLabXMLRepo(vt,"TemaLaboratorXML.xml");
+        NotaXMLRepo ntrepo=new NotaXMLRepo(vn,"NotaXML.xml");
+        StudentXMLService stsrv=new StudentXMLService(strepo);
+        TemaLabXMLService tmsrv=new TemaLabXMLService(tmrepo);
+        NotaXMLService ntsrv=new NotaXMLService(ntrepo);
+        String[] params={"22","nume1","grupa1","email@mail.com","prof"};
+        try {
+            stsrv.add(params);
+            assertTrue(true);
+        } catch (ValidatorException e){
+            assertTrue(false);
+        }
+    }
+}
